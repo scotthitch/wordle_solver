ae9768048380a5cf10a09fc3b0abda625caaca30
Scott Hitchcock <scotthitch5@gmail.com>
2024-06-04 21:30:52 +1200

    restructure for testing

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,12 @@
-import unittest
+import pytest
 from src import solver
 
-class TestSolver(unittest.TestCase):
-    def runTest(self):
-        # solver.
-        
+def test_answer():
+    result = [('s', solver.Result.INCORRECT),
+              ('l', solver.Result.INCORRECT),
+              ('a', solver.Result.INCORRECT),
+              ('t', solver.Result.INCORRECT),
+              ('e', solver.Result.INCORRECT)]
+    
+    word_to_test = "guild"
+    assert solver.word_meets_criteria(word_to_test, result) != True
